c9b5c7d9f5a0bb731c8dbf0654db42d706961457
ShamyakGoel <sj907822@gmail.com>
2022-03-22 12:32:02 +0530

    Learned about Image component and made changes to huntingcoder project

--- huntingcoder/pages/index.js
+++ huntingcoder/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Script from 'next/script'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -12,7 +11,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         {/* <Script  src="./sc.js"></Script> */}
       </Head>
-      <Script strategy='lazyOnload' src="./sc.js"></Script>
+      <nav className={styles.mainnav}>
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Blog</li>
+              <li>Contact</li>
+          </ul>
+      </nav>
       <main className={styles.main}>
         <h1 className={styles.title}>
         Hunting coder
@@ -22,35 +28,21 @@ export default function Home() {
           A blog for the hunting coders by a hunting coder
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+       <div className="blogs">
+           <h2>Popular Blogs</h2>
+        <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>Javascript is the langauage used to describe to logic for the web</p>
         </div>
+        <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>Javascript is the langauage used to describe to logic for the web</p>
+        </div>
+        <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>Javascript is the langauage used to describe to logic for the web</p>
+        </div>
+       </div>
       </main>
 
       <footer className={styles.footer}>
